366b4d4066d9aface0434760b0a7ff1a9ee30268
Maksym <Crashdey323@gmail.com>
2019-11-03 22:50:15 +0200

    Started keeping a version history

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        className="logo"
+        src="https://upload.wikimedia.org/wikipedia/ru/a/a3/Unity_Logo.png"
+        alt="lorem"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="http://we.com.mk/wp-content/uploads/2016/12/greece-islands.jpg"></img>
+      </div>
+      <div>
+        <div img="https://static-cdn.123rf.com/images/v5/index-thumbnail/84170952-b.jpg"></div>
+        <div>ava + description</div>
+        <div>
+          My posts
+          <div>New post</div>
+          <diw>
+            <diw>post 1</diw>
+            <diw>post 2</diw>
+          </diw>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
