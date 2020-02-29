52eab944eab0064e1c353ad676bf87cbefc4ed35
Maksym Maiboroda <crashdey323@gmail.com>
2020-03-01 00:32:57 +0200

    Updates

---
+++ precache-manifest.44126a53f707943ec284b337cf8eeb32.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be05e6b7e45255862607efd4f1e650f",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/index.html"
+  },
+  {
+    "revision": "33cfaf0ece15660bc4ef",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/css/2.04631b38.chunk.css"
+  },
+  {
+    "revision": "bd0a0600d2eb00431ca5",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/css/3.fecafe9a.chunk.css"
+  },
+  {
+    "revision": "08468d672a58919b56d2",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/css/4.658f0e49.chunk.css"
+  },
+  {
+    "revision": "e2d90c392e466f5522b3",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/css/main.d786a893.chunk.css"
+  },
+  {
+    "revision": "33cfaf0ece15660bc4ef",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/js/2.69079535.chunk.js"
+  },
+  {
+    "revision": "bd0a0600d2eb00431ca5",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/js/3.93067e95.chunk.js"
+  },
+  {
+    "revision": "08468d672a58919b56d2",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/js/4.ed91af68.chunk.js"
+  },
+  {
+    "revision": "e2d90c392e466f5522b3",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/js/main.06cb54a8.chunk.js"
+  },
+  {
+    "revision": "7bcb997dbae9c1251a6e",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/js/runtime-main.ec8d2709.js"
+  },
+  {
+    "revision": "3a9069195c702979546d4bf14b408868",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/media/Logo.3a906919.svg"
+  },
+  {
+    "revision": "a3fd5e05e96ffe182b10d6d401776a82",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/media/Preloader.a3fd5e05.svg"
+  },
+  {
+    "revision": "1f20781a9392c2baeb02aedd5456ebbf",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/media/avaCat.1f20781a.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "413f66020fe57cd1b530266ff33fa595",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/media/iconUser.413f6602.png"
+  },
+  {
+    "revision": "0427aa0ef8ecbd8ba0d5fdc1be515113",
+    "url": "/react-gh-pages/maksimMaiboroda/my-prpject-/static/media/imgPageDevelopment.0427aa0e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-prpject-/precache-manifest.ee2425d777babad81ac6c66cd1aebbb5.js"
+  "/react-gh-pages/maksimMaiboroda/my-prpject-/precache-manifest.44126a53f707943ec284b337cf8eeb32.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-prpject-/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/maksimMaiboroda/my-prpject-/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
